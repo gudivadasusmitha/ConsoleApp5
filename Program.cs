bcd8247b96f9ed2b0392991d833f24f7c057c112
gudivadasusmitha <Gudivada.Susmitha@ibm.com>
2022-03-01 15:32:28 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                int i, num = 20, sum = 0;
+
+                i = 1;
+                while (i <= num)
+                {
+                    sum = sum + i;
+                    i++;
+                }
+
+                Console.WriteLine("Sum of first" + num +
+                " natural numbers = " + sum);
+            }
+        }
+    }
+    
+}
